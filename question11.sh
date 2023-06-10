0e864157b42f37df56137648bebdf05d1a3103cc
Skyler023 <124237070+Skyler023@users.noreply.github.com>
2023-06-10 21:36:41 +0930

    Update question11.sh

--- question11.sh
+++ question11.sh
@@ -1,17 +1,10 @@
-# Create and switch to a new branch named branch2
+#!/bin/bash
+
 git checkout -b branch2
-
-# Create a file named file4
 touch file4
-
-# Add file4 to the staging area
 git add file4
-
-# Commit file4
 git commit -m "Add file4"
-
-# Modify file4
 echo "Modified content" >> file4
-
-# Switch back to the main branch without committing changes
-git checkout -
+git stash
+git checkout main
+git stash apply
