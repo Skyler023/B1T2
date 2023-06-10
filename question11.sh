293bf97a6d39dc087884c82178b7e29d70b6f070
Skyler023 <124237070+Skyler023@users.noreply.github.com>
2023-06-10 21:33:44 +0930

    Update question11.sh

--- question11.sh
+++ question11.sh
@@ -1,11 +1,17 @@
-#!/bin/bash
-
+# Create and switch to a new branch named branch2
 git checkout -b branch2
 
+# Create a file named file4
 touch file4
+
+# Add file4 to the staging area
 git add file4
+
+# Commit file4
 git commit -m "Add file4"
 
-# Make your modifications to file4 here
+# Modify file4
+echo "Modified content" >> file4
 
-git checkout main
+# Switch back to the main branch without committing changes
+git checkout -
