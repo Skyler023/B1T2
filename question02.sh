84d2ac790641ec8032f374f7413e67511710fc78
Skyler023 <124237070+Skyler023@users.noreply.github.com>
2023-06-10 19:29:01 +0930

    Create question02.sh

---
+++ question02.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Initialize a new Git repository
+git init
+
+# Create a sample file
+touch README.md
+
+# Add the file to the repository
+git add README.md
+
+# Commit the changes
+git commit -m "Initial commit"
