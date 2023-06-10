e7de6cd555b4b3f79022bf2069c4470eb92b20f2
Skyler023 <124237070+Skyler023@users.noreply.github.com>
2023-06-10 21:16:17 +0930

    Update question08.sh

--- question08.sh
+++ question08.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-
+# Create a directory named dir2
 mkdir dir2
-mv *.txt dir2/
-git add dir2/*.txt
-git commit -m "Move text files to dir2"
 
+# Move all text files from the current directory to dir2
+mv *.txt dir2/
+
+# Stage all modified files
+git add -u
+
+# Commit the staged changes
+git commit -m "Committing modified files"
