67ae7e69d3c0e545132ba60580debae27ea4b3c4
Skyler023 <124237070+Skyler023@users.noreply.github.com>
2023-06-10 22:39:54 +0930

    Update question08.sh

--- question08.sh
+++ question08.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 # Create the directory and file
 mkdir dir2
 git add dir2
@@ -7,4 +6,3 @@ mv *.txt dir2/
 # Stage the directory and its contents
 git add dir2/*.txt
 git commit -m "Move text files to dir2"
-git push
