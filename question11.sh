186a42bcd27e8e3e873fcd03921e055dfdedf8b3
Skyler023 <124237070+Skyler023@users.noreply.github.com>
2023-06-10 20:34:00 +0930

    Update question11.sh

--- question11.sh
+++ question11.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 
-# Create and switch to branch2
-git checkout -b branch2
-
-# Create and commit file4
-echo "This is file4" > file4
-git add file4
-git commit -m "Add file4"
-
-# Modify file4
-echo "This is a modified version of file4" > file4
-
-# Switch back to the main branch
+# Switch to the main branch
 git checkout main
+
+# Merge branch1 into main
+git merge branch1
